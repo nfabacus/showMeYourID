7eaa20850e9a591c4f752cef6646698719733db3
noby <n.fujioka@abacusmaths.info>
2016-06-30 13:23:12 +0100

    3rd test passed

--- lib/showMe.rb
+++ lib/showMe.rb
@@ -1,4 +1,5 @@
 def show_me(name)
   return 'Francis is a name !' if name == 'Francis'
   return 'Jean-Eulard is a name !' if name == 'Jean-Eulard'
+  return '123 is not a name !'
 end
--- spec/showMeYourID_spec.rb
+++ spec/showMeYourID_spec.rb
@@ -7,4 +7,7 @@ describe 'show_me' do
   it 'returns "Jean-Eulard is a name !" when passed "Jean-Eulard"' do
     expect(show_me('Jean-Eulard')).to eq 'Jean-Eulard is a name !'
   end
+  it 'returns "123 is not a name !" when passed "123"' do
+    expect(show_me('123')).to eq '123 is not a name !'
+  end
 end

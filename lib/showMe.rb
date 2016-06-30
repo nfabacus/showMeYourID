001e1f29e77d3f7a0a5b2aeb0015d302c633a3c2
noby <n.fujioka@abacusmaths.info>
2016-06-30 12:48:15 +0100

    passed the first test.

---
+++ lib/showMe.rb
@@ -0,0 +1,3 @@
+def show_me(name)
+  'Francis is a name !'
+end
--- spec/showMeYourID_spec.rb
+++ spec/showMeYourID_spec.rb
@@ -1,3 +1,5 @@
+require 'showMe'
+
 describe 'show_me' do
   it 'returns "Francis is a name !" when passed "Francis"' do
     expect(show_me('Francis')).to eq 'Francis is a name !'

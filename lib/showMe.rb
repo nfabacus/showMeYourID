80a234a9c465ebd82a2a8d6d96eebd01835985b3
noby <n.fujioka@abacusmaths.info>
2016-07-01 22:29:23 +0100

    final change

--- lib/showMe.rb
+++ lib/showMe.rb
@@ -3,14 +3,7 @@ def show_me(name)
   return false if name[-1].match(/[-]/)
   return false if name[0].match(/[A-Z]/).nil?
   return false if !name.match(/--/).nil?
-
   name_array = name.split('-')
-
   name_array.each {|name| return false if name.empty? || name != name.capitalize }
-
-  # return true if name_array[0, 1].match(/[A-Z]/)
-  # return true if name.match(/[-]/)
-  # return true if name[0].match(/[A-Z]/)
   return true
-  #return true if name.ma-tch
 end
